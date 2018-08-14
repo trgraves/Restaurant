748eff01a6faf420f65871bb91275b647c1015fb
Thomas Graves <trgraves90@gmail.com>
2018-08-13 21:55:38 -0400

    Added validation to form

--- Restaurant/js/validation.js
+++
@@ -1,9 +0,0 @@
-var form = document.querySelector(".needs-validation");
-
-form.addEventListener("submit", function(event)){
-  if (form.checkValidity() === false){
-    event.preventDefault();
-    event.stopPropagation();
-  }
-  form.classList.add("was-validated");
-}
